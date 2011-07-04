66ba4bf8fb7ebb479f318fa2f0fde53b4681ee8c
Masaki Takegami <m_takegami@spicysoft.com>
2011-07-04 18:41:37 +0900

    clean JSLint

--- src/main/js/Reader.js
+++ src/main/js/Reader.js
@@ -14,7 +14,7 @@ function $Reader(_member) {
   var API_ROOT = '/api';
   var width = 0;
   var height = 0;
-  
+
   var storyId;
   var storyMetaFile;
   var scenes;
@@ -48,6 +48,36 @@ function $Reader(_member) {
   }
   var SceneAnimator = new $SceneAnimator(width, height, FPS);
 
+  
+  /**
+   * 画面描画
+   *
+   * @return void
+   */
+  var paint = function() {
+    var i = sceneImages[currentSceneIndex];
+    if (i === undefined || !i.hasLoaded()) {
+      return;
+    }
+
+    var x = SceneAnimator.x();
+    var y = SceneAnimator.y();
+    var w = i.width;
+    var h = i.height;
+    var dx = (width - w) / 2 + x;
+    var dy = (height - h) / 2 + y;
+          
+    if (App.IE) {
+      i.style.cssText = "position: absolute; top: " + dy + "px; left:" + dx + "px;";
+      $("#canvas").empty().append(i);
+    } else {
+      var context = canvas.getContext("2d");
+      context.fillStyle = canvas.style.background;
+      context.fillRect(0, 0, width, height);
+      context.drawImage(i, dx, dy);
+    }
+  };
+  
   /**
    * メニューを表示する
    */
@@ -113,7 +143,6 @@ function $Reader(_member) {
     $("#progress_current").text(currentSceneIndex+1);
   };
 
-
   /**
    * 表示モードを通信エラーに切り替える
    */
@@ -125,97 +154,6 @@ function $Reader(_member) {
     $("#error").show();
   };
 
-  /**
-   * 表示モードを「マンガ閲覧中」に切り替える
-   */
-  var showReader = function() {
-          console.log("showReader");
-    $("#loading").hide();
-    $("#reader").show();
-    $("#finish").hide();
-    $("#error").hide();
-    $("#canvas").unbind('mouseup');
-    $("#canvas").mouseup(function(event) {
-      goNext();
-      if (App.IE) {
-        event.returnValue = false;
-      }else{
-        event.preventDefault();
-      }
-    });
-    var format = _("Loading images");
-    var args = {
-      "story_number"   : storyMetaFile.story_number,
-      "story_title"    : storyMetaFile.story_title,
-      "number_of_story": storyMetaFile.number_of_story
-    };
-  };
-
-  /**
-   * 指定したマンガの話をリーダーで開く。
-   */
-  var openStory = function(_storyId) {
-    if (sceneImages !== undefined) {
-      for ( var n = 0; n < sceneImages.length; n++) {
-        var i = sceneImages[n];
-        if (i !== undefined) {
-          i.onload = null;
-        }
-      }
-    }
-    sceneImages = [];
-    storyId = _storyId;
-    scenes = null;
-    currentSceneIndex = 0;
-    storyMetaFile = null;
-
-    showLoading();
-    apiStoryMetaFile(storyId, function(json) {
-      storyMetaFile = json;
-      scenes = storyMetaFile.scenes;
-      updateSceneCount();
-      if (scenes.length === 0) {
-        showFinished();
-      } else {
-        showReader();
-        jumpToScene(0);
-      }
-    }, function() {
-      showError();
-    });
-  };
-
-  /**
-   * [サーバーAPIとの通信メソッド] サーバーからシーン画像を取得する
-   *
-   * @private
-   * @return Image ただし非同期なので読み込み完了していることは保証されない
-   */
-  var apiSceneImage = function(sceneId) {
-    var i = new Image();
-    i.hasLoaded = function(){
-      //IE9でImage.completeが動作しない場合があるので、Image.widthを見て
-      //ロードが完了したか判断する
-      return this.width > 0;
-    };
-    i.src = API_ROOT + '/sceneImage/' + sceneId;
-    return i;
-  };
-  
-  /**
-   * 指定したシーン番号のシーン読み込みを準備する。 またこの内部でプリロード、破棄処理も行う。 現在は一括ロード。
-   */
-  var fetchSceneImage = function(sceneIndex) {
-    var under = sceneIndex - 1;
-    var prefetch = sceneIndex + 5;
-    for ( var n = 0; n < scenes.length; n++) {
-      if (n < under && sceneImages[n] !== undefined) {
-        sceneImages[n] = undefined;
-      } else if (n <= prefetch && sceneImages[n] === undefined) {
-        sceneImages[n] = apiSceneImage(scenes[n].id);
-      }
-    }
-  };
 
   var showFinished = function() {
     var next_story_id = storyMetaFile.next_story_id;
@@ -281,37 +219,36 @@ function $Reader(_member) {
   };
   
   /**
-   * 画面描画
+   * [サーバーAPIとの通信メソッド] サーバーからシーン画像を取得する
    *
-   * @return void
+   * @private
+   * @return Image ただし非同期なので読み込み完了していることは保証されない
    */
-  var paint = function() {
-    console.log("paint");
-    var i = sceneImages[currentSceneIndex];
-    if (i === undefined || !i.hasLoaded()) {
-      return;
-    }
-
-    var x = SceneAnimator.x();
-    var y = SceneAnimator.y();
-          console.log("x:" + x + " y:" + y);
-    var w = i.width;
-    var h = i.height;
-    var dx = (width - w) / 2 + x;
-    var dy = (height - h) / 2 + y;
-          console.log("dx:" + dx + " dy:" + dy);
-          
-    if (App.IE) {
-      i.style.cssText = "position: absolute; top: " + dy + "px; left:" + dx + "px;";
-      $("#canvas").empty().append(i);
-    } else {
-      var context = canvas.getContext("2d");
-      context.fillStyle = canvas.style.background;
-      context.fillRect(0, 0, width, height);
-      context.drawImage(i, dx, dy);
+  var apiSceneImage = function(sceneId) {
+    var i = new Image();
+    i.hasLoaded = function(){
+      //IE9でImage.completeが動作しない場合があるので、Image.widthを見て
+      //ロードが完了したか判断する
+      return this.width > 0;
+    };
+    i.src = API_ROOT + '/sceneImage/' + sceneId;
+    return i;
+  };
+  
+  /**
+   * 指定したシーン番号のシーン読み込みを準備する。 またこの内部でプリロード、破棄処理も行う。 現在は一括ロード。
+   */
+  var fetchSceneImage = function(sceneIndex) {
+    var under = sceneIndex - 1;
+    var prefetch = sceneIndex + 5;
+    for ( var n = 0; n < scenes.length; n++) {
+      if (n < under && sceneImages[n] !== undefined) {
+        sceneImages[n] = undefined;
+      } else if (n <= prefetch && sceneImages[n] === undefined) {
+        sceneImages[n] = apiSceneImage(scenes[n].id);
+      }
     }
   };
-
   
   /**
    * コマ毎の初期化
@@ -349,7 +286,6 @@ function $Reader(_member) {
       i.onload = onloaded;
     }
   };
-  
   /**
    * 次のシーンへ進める
    */
@@ -361,7 +297,6 @@ function $Reader(_member) {
       showFinished();
     }
   };
-
 
   /**
    * アニメーションの各フレーム処理の実行
@@ -399,6 +334,67 @@ function $Reader(_member) {
       throw "Illega state";
     }
   };
+  
+  /**
+   * 表示モードを「マンガ閲覧中」に切り替える
+   */
+  var showReader = function() {
+          console.log("showReader");
+    $("#loading").hide();
+    $("#reader").show();
+    $("#finish").hide();
+    $("#error").hide();
+    $("#canvas").unbind('mouseup');
+    $("#canvas").mouseup(function(event) {
+      goNext();
+      if (App.IE) {
+        event.returnValue = false;
+      }else{
+        event.preventDefault();
+      }
+    });
+    var format = _("Loading images");
+    var args = {
+      "story_number"   : storyMetaFile.story_number,
+      "story_title"    : storyMetaFile.story_title,
+      "number_of_story": storyMetaFile.number_of_story
+    };
+  };
+
+  /**
+   * 指定したマンガの話をリーダーで開く。
+   */
+  var openStory = function(_storyId) {
+    if (sceneImages !== undefined) {
+      console.log(sceneImages);
+      for ( var n = 0; n < sceneImages.length; n++) {
+        var i = sceneImages[n];
+        if (i !== undefined) {
+          i.onload = null;
+        }
+      }
+    }
+    sceneImages = [];
+    storyId = _storyId;
+    scenes = null;
+    currentSceneIndex = 0;
+    storyMetaFile = null;
+
+    showLoading();
+    apiStoryMetaFile(storyId, function(json) {
+      storyMetaFile = json;
+      scenes = storyMetaFile.scenes;
+      updateSceneCount();
+      if (scenes.length === 0) {
+        showFinished();
+      } else {
+        showReader();
+        jumpToScene(0);
+      }
+    }, function() {
+      showError();
+    });
+  };
 
   /**
    * スクロールをひとつ前に戻す
@@ -423,7 +419,7 @@ function $Reader(_member) {
   var goToFirst = function(){
 
   };
-
+  
   this.openStory = openStory;
 }
 
